99db1d31f680001ea7478170d771b64b62b32184
udaykumar-m <udaykumardevil3830@gmail.com>
2024-09-07 00:23:35 +0530

    Listed the Audio files with file permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Songs(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class Songs extends StatefulWidget {
-  const Songs({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  _SongsState createState() => _SongsState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AudioFilesScreen(),
+    );
+  }
 }
 
-class _SongsState extends State<Songs> {
-  // Main method.
-  final OnAudioQuery _audioQuery = OnAudioQuery();
+class AudioFilesScreen extends StatefulWidget {
+  @override
+  _AudioFilesScreenState createState() => _AudioFilesScreenState();
+}
 
-  // Indicate if application has permission to the library.
-  bool _hasPermission = false;
+class _AudioFilesScreenState extends State<AudioFilesScreen> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+  List<SongModel> _songs = [];
 
   @override
   void initState() {
     super.initState();
-    // (Optinal) Set logging level. By default will be set to 'WARN'.
-    //
-    // Log will appear on:
-    //  * XCode: Debug Console
-    //  * VsCode: Debug Console
-    //  * Android Studio: Debug and Logcat Console
-    LogConfig logConfig = LogConfig(logType: LogType.DEBUG);
-    _audioQuery.setLogConfig(logConfig);
-
-    // Check and request for permission.
-    checkAndRequestPermissions();
+    _fetchAudioFiles();
   }
 
-  checkAndRequestPermissions({bool retry = false}) async {
-    // The param 'retryRequest' is false, by default.
-    _hasPermission = await _audioQuery.checkAndRequest(
-      retryRequest: retry,
-    );
+  Future<void> _fetchAudioFiles() async {
+    // Request storage permission
+    if (await _requestPermission()) {
+      // Query all songs on the device
+      List<SongModel> songs = await _audioQuery.querySongs();
+      setState(() {
+        _songs = songs;
+      });
+    }
+  }
 
-    // Only call update the UI if application has all required permissions.
-    _hasPermission ? setState(() {}) : null;
+  Future<bool> _requestPermission() async {
+    PermissionStatus status = await Permission.storage.request();
+    return status.isGranted;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("OnAudioQueryExample"),
-        elevation: 2,
+        title: Text("Audio Files"),
       ),
-      body: Center(
-        child: !_hasPermission
-            ? noAccessToLibraryWidget()
-            : FutureBuilder<List<SongModel>>(
-                // Default values:
-                future: _audioQuery.querySongs(
-                  sortType: null,
-                  orderType: OrderType.ASC_OR_SMALLER,
-                  uriType: UriType.EXTERNAL,
-                  ignoreCase: true,
-                ),
-                builder: (context, item) {
-                  // Display error, if any.
-                  if (item.hasError) {
-                    return Text(item.error.toString());
-                  }
-
-                  // Waiting content.
-                  if (item.data == null) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  // 'Library' is empty.
-                  if (item.data!.isEmpty) return const Text("Nothing found!");
-
-                  // You can use [item.data!] direct or you can create a:
-                  // List<SongModel> songs = item.data!;
-                  return ListView.builder(
-                    itemCount: item.data!.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(item.data![index].title),
-                        subtitle: Text(item.data![index].artist ?? "No Artist"),
-                        trailing: const Icon(Icons.arrow_forward_rounded),
-                        // This Widget will query/load image.
-                        // You can use/create your own widget/method using [queryArtwork].
-                        leading: QueryArtworkWidget(
-                          controller: _audioQuery,
-                          id: item.data![index].id,
-                          type: ArtworkType.AUDIO,
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-      ),
-    );
-  }
-
-  Widget noAccessToLibraryWidget() {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.redAccent.withOpacity(0.5),
-      ),
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text("Application doesn't have access to the library"),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () => checkAndRequestPermissions(retry: true),
-            child: const Text("Allow"),
-          ),
-        ],
-      ),
+      body: _songs.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _songs.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_songs[index].title),
+                  subtitle: Text(_songs[index].artist ?? "Unknown Artist"),
+                );
+              },
+            ),
     );
   }
 }
