07e84de6c8967acba9112ddf648f540e27af29ce
udaykumar-m <udaykumardevil3830@gmail.com>
2024-09-12 21:36:46 +0530

    Ajay suggestions on audio player making a seperate widget class

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,104 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: AudioFilesScreen(),
     );
   }
 }
 
 class AudioFilesScreen extends StatefulWidget {
+  const AudioFilesScreen({super.key});
+
   @override
   _AudioFilesScreenState createState() => _AudioFilesScreenState();
 }
 
-class _AudioFilesScreenState extends State<AudioFilesScreen>
-    with WidgetsBindingObserver {
+class _AudioFilesScreenState extends State<AudioFilesScreen> {
   final OnAudioQuery _audioQuery = OnAudioQuery();
-  final AudioPlayer _audioPlayer = AudioPlayer();
   List<SongModel> _songs = [];
-  int? _currentSongIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAudioFiles();
+  }
+
+  Future<void> _fetchAudioFiles() async {
+    if (await _requestPermissions()) {
+      List<SongModel> songs = await _audioQuery.querySongs();
+      setState(() {
+        _songs = songs;
+      });
+    }
+  }
+
+  Future<bool> _requestPermissions() async {
+    if (await Permission.storage.isGranted) {
+      return true;
+    }
+
+    if (await Permission.audio.request().isGranted) {
+      return true;
+    } else {
+      openAppSettings();
+    }
+
+    if (await Permission.storage.isPermanentlyDenied) {
+      openAppSettings();
+      return false;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio Files'),
+      ),
+      body: _songs.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _songs.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_songs[index].title),
+                  subtitle: Text(_songs[index].artist ?? "Unknown Artist"),
+                  onTap: () => _openPlayer(context, _songs[index]),
+                );
+              },
+            ),
+    );
+  }
+
+  void _openPlayer(BuildContext context, SongModel song) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => AudioPlayerBottomSheet(song: song),
+    );
+  }
+}
+
+class AudioPlayerBottomSheet extends StatefulWidget {
+  final SongModel song;
+
+  const AudioPlayerBottomSheet({super.key, required this.song});
+
+  @override
+  _AudioPlayerBottomSheetState createState() => _AudioPlayerBottomSheetState();
+}
+
+class _AudioPlayerBottomSheetState extends State<AudioPlayerBottomSheet> {
+  final AudioPlayer _audioPlayer = AudioPlayer();
   bool _isPlaying = false;
   Duration _currentPosition = Duration.zero;
   Duration _songDuration = Duration.zero;
@@ -34,8 +109,16 @@ class _AudioFilesScreenState extends State<AudioFilesScreen>
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    _fetchAudioFiles();
+    _initializePlayer();
+  }
+
+  void _initializePlayer() async {
+    _audioPlayer.setSource(DeviceFileSource(widget.song.data)).then((val) {
+      _audioPlayer.resume();
+      setState(() {
+        _isPlaying = true;
+      });
+    });
     _audioPlayer.onPositionChanged.listen((Duration newPosition) {
       setState(() {
         _currentPosition = newPosition;
@@ -49,115 +132,64 @@ class _AudioFilesScreenState extends State<AudioFilesScreen>
     });
   }
 
+  void _playPauseAudio() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    } else {
+      await _audioPlayer.resume();
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  void _seekAudio(double value) async {
+    final newPosition = Duration(seconds: value.toInt());
+    await _audioPlayer.seek(newPosition);
+  }
+
   @override
   void dispose() {
-    WidgetsBinding.instance
-        .removeObserver(this); // Remove observer to avoid memory leaks
     _audioPlayer.dispose();
     super.dispose();
   }
 
+  String formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return '$minutes:$seconds';
+  }
+
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _fetchAudioFiles(); // Refresh song list when the app comes to the foreground
-    }
-  }
-
-  Future<void> _fetchAudioFiles() async {
-    if (await _requestPermission()) {
-      List<SongModel> songs = await _audioQuery.querySongs();
-      setState(() {
-        _songs = songs;
-      });
-    }
-  }
-
-  Future<bool> _requestPermission() async {
-    if (await Permission.storage.isGranted) {
-      return true;
-    }
-
-    if (await Permission.audio.request().isGranted) {
-      return true;
-    }
-
-    if (await Permission.storage.isPermanentlyDenied) {
-      openAppSettings();
-      return false;
-    }
-    return false;
-  }
-
-  void _playPauseAudio(String songPath) async {
-    if (_isPlaying && _currentSongIndex != null) {
-      await _audioPlayer.pause();
-      setState(() {
-        _isPlaying = false;
-      });
-    } else {
-      await _audioPlayer.play(DeviceFileSource(songPath));
-      setState(() {
-        _isPlaying = true;
-      });
-    }
-  }
-
-  void _nextSong() {
-    if (_currentSongIndex != null && _currentSongIndex! < _songs.length - 1) {
-      _currentSongIndex = _currentSongIndex! + 1;
-      _playPauseAudio(_songs[_currentSongIndex!].data);
-    }
-  }
-
-  void _previousSong() {
-    if (_currentSongIndex != null && _currentSongIndex! > 0) {
-      _currentSongIndex = _currentSongIndex! - 1;
-      _playPauseAudio(_songs[_currentSongIndex!].data);
-    }
-  }
-
-  void _openPlayer(BuildContext context, int songIndex) {
-    _currentSongIndex = songIndex;
-    _playPauseAudio(_songs[songIndex].data);
-
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => _musicPlayerUI(context, songIndex),
-    );
-  }
-
-  Widget _musicPlayerUI(BuildContext context, int songIndex) {
+  Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        SizedBox(height: 20),
-        QueryArtworkWidget(
-          id: _songs[songIndex].id,
-          type: ArtworkType.AUDIO,
-          nullArtworkWidget: Icon(Icons.music_note, size: 100),
-          artworkFit: BoxFit.cover,
-        ),
-        SizedBox(height: 10),
+        // SizedBox(height: 20),
+        // QueryArtworkWidget(
+        //   id: widget.song.id,
+        //   type: ArtworkType.AUDIO,
+        //   nullArtworkWidget: Icon(Icons.music_note, size: 100),
+        //   artworkFit: BoxFit.cover,
+        // ),
+        const SizedBox(height: 10),
         Text(
-          _songs[songIndex].title,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          widget.song.title,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
         ),
         Text(
-          _songs[songIndex].artist ?? "Unknown Artist",
-          style: TextStyle(fontSize: 18, color: Colors.grey),
+          widget.song.artist ?? "Unknown Artist",
+          style: const TextStyle(fontSize: 18, color: Colors.grey),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Slider(
           value: _currentPosition.inSeconds.toDouble(),
           min: 0,
           max: _songDuration.inSeconds.toDouble(),
-          onChanged: (double value) async {
-            final newPosition = Duration(seconds: value.toInt());
-            await _audioPlayer.seek(newPosition);
-            setState(() {
-              _currentPosition = newPosition;
-            });
+          onChanged: (double value) {
+            _seekAudio(value);
           },
         ),
         Padding(
@@ -175,54 +207,25 @@ class _AudioFilesScreenState extends State<AudioFilesScreen>
           children: [
             IconButton(
               iconSize: 36,
-              icon: Icon(Icons.skip_previous),
-              onPressed: _previousSong,
+              icon: const Icon(Icons.skip_previous),
+              onPressed: () {}, // Add skip logic if needed
             ),
             IconButton(
               iconSize: 48,
               icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-              onPressed: () => _playPauseAudio(_songs[songIndex].data),
+              onPressed: () {
+                _playPauseAudio();
+              },
             ),
             IconButton(
               iconSize: 36,
-              icon: Icon(Icons.skip_next),
-              onPressed: _nextSong,
+              icon: const Icon(Icons.skip_next),
+              onPressed: () {}, // Add skip logic if needed
             ),
           ],
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
       ],
-    );
-  }
-
-  String formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return '$minutes:$seconds';
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Audio Files"),
-      ),
-      body: RefreshIndicator(
-        onRefresh: _fetchAudioFiles,
-        child: _songs.isEmpty
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: _songs.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_songs[index].title),
-                    subtitle: Text(_songs[index].artist ?? "Unknown Artist"),
-                    onTap: () => _openPlayer(context, index),
-                  );
-                },
-              ),
-      ),
     );
   }
 }
